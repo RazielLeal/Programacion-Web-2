9dd1f73fee06c31e5b0944d02742dd89062f4022
RazielLeal <raziel-leal99@outlook.com>
2025-10-22 20:09:15 -0600

    FrontEnd
    Creación de Landing Page

--- Proyecto/Front/src/App.js
+++ Proyecto/Front/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Register from './Paginas/Register';
 import Login from './Paginas/Login';
-import Home from './Paginas/Home';
+import Landing from './Paginas/Landing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/Register" element={<Register />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/Login" element={<Login />}></Route>        
+        <Route path="/Login" element={<Landing />}></Route>
+
       </Routes>
     </BrowserRouter>
   );
---
+++ Proyecto/Front/src/Paginas/Landing.jsx
@@ -0,0 +1,39 @@
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import "./CSS/Landing.css";
+
+
+
+function Home() {
+  // const nameUser = localStorage.getItem("user");
+
+  const navigate = useNavigate();
+  
+  const Loginredhome = () => {    
+    navigate("/login");
+  };
+
+  return (
+    <>
+      {/* <Navbar /> */}
+      <main className="main-home">
+        <div className="centered-container">
+          <div className="title-home">ARTCONNECT</div>
+          <button onClick={Loginredhome} className="login-red-home" >COMIENZA TU EXPERIENCIA</button>
+        </div>
+
+        <div className="mujer-izq"></div>
+        <div className="mujer2"></div>
+        <div className="cuadro3"></div>
+        <div className="cuadro4"></div>
+        <div className="monito"></div>
+        <div className="rope"></div>
+        <div className="estatua"></div>
+        <footer className="footer-home"></footer>
+      </main>
+    </>
+  );
+}
+
+export default Home;
